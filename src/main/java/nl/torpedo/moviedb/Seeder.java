cd6cdbfc7f7b99e4ea4f87291459178bf67bcb66
Thomas Vrielink <thomasvrielink92@gmail.com>
2023-11-09 15:11:00 +0100

    seed reviews in seeder

--- src/main/java/nl/torpedo/moviedb/Seeder.java
+++ src/main/java/nl/torpedo/moviedb/Seeder.java
@@ -9,6 +9,9 @@ public class Seeder implements CommandLineRunner {
     private final MovieRepository movieRepository;
     private final ReviewRepository reviewRepository;
 
+    private final Movie ts3 = new Movie("Toy Story 3", "Cry your heart out");
+    private final Movie lotr = new Movie("The Lord of the Rings: Return of the King", "EPIIIIIIIIIIIIIIIC!");
+
     public Seeder(MovieRepository movieRepository, ReviewRepository reviewRepository) {
         this.movieRepository = movieRepository;
         this.reviewRepository = reviewRepository;
@@ -20,11 +23,14 @@ public class Seeder implements CommandLineRunner {
     }
 
     public void seedMovies() {
-        movieRepository.save(new Movie("Toy Story 3", "Cry your heart out"));
-        movieRepository.save(new Movie("The Lord of the Rings: Return of the King", "EPIIIIIIIIIIIIIIIC!"));
+
+        movieRepository.save(ts3);
+        movieRepository.save(lotr);
     }
 
     public void seedReviews() {
 
+        reviewRepository.save(new Review(ts3, 5, "Fantastisch!"));
+        reviewRepository.save(new Review(lotr, 5, "EPIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIC!"));
     }
 }
