43ce449ecf2727babdfcabaed1e3535656fb46d2
Thomas Vrielink <thomasvrielink92@gmail.com>
2023-11-09 15:29:58 +0100

    added lotr1 and lotr2 to seeder

--- src/main/java/nl/torpedo/moviedb/Seeder.java
+++ src/main/java/nl/torpedo/moviedb/Seeder.java
@@ -10,6 +10,9 @@ public class Seeder implements CommandLineRunner {
     private final ReviewRepository reviewRepository;
 
     private final Movie ts3 = new Movie("Toy Story 3", "Cry your heart out");
+
+    private final Movie lotr1 = new Movie("The Lord of the Rings: The FellowShip of the Ring", "EPIIIIIIIIIIIIIIIC!");
+    private final Movie lotr2 = new Movie("The Lord of the Rings: The Two Towers", "EPIIIIIIIIIIIIIIIC!");
     private final Movie lotr3 = new Movie("The Lord of the Rings: The Return of the King", "EPIIIIIIIIIIIIIIIC!");
 
     public Seeder(MovieRepository movieRepository, ReviewRepository reviewRepository) {
@@ -25,6 +28,8 @@ public class Seeder implements CommandLineRunner {
     public void seedMovies() {
 
         movieRepository.save(ts3);
+        movieRepository.save(lotr1);
+        movieRepository.save(lotr2);
         movieRepository.save(lotr3);
     }
 
