5bf62808f11b812efa942cde6ee685181e49a28c
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2023-11-09 14:49:55 +0100

    Linking Movie to Reviews

--- src/main/java/nl/torpedo/moviedb/Movie.java
+++ src/main/java/nl/torpedo/moviedb/Movie.java
@@ -3,6 +3,9 @@ package nl.torpedo.moviedb;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.Set;
 
 @Entity
 public class Movie {
@@ -13,12 +16,23 @@ public class Movie {
     private String name;
     private String description;
 
+    @OneToMany(mappedBy = "movie")
+    private Set<Review> reviews;
+
     public String getName() {
         return name;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public Set<Review> getReviews() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        reviews.add(review);
     }
 
     public Movie(String name, String description) {
